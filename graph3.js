baad8bdb56422f54bd1a874958a5035cb2851556
Popole <falqleopaul@gmail.com>
2024-01-12 16:28:54 +0100

    graph 3 qui marche

--- graph3.js
+++ graph3.js
@@ -1,7 +1,7 @@
 function getTreeMap(){
     var margin = {top: 10, right: 10, bottom: 10, left: 10},
-        width = 445 - margin.left - margin.right,
-        height = 445 - margin.top - margin.bottom;
+        width = 1000 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
     var svg = d3.select(".graph3")
@@ -13,47 +13,48 @@ function getTreeMap(){
             "translate(" + margin.left + "," + margin.top + ")");
 
 // Read data
-    d3.csv('https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_hierarchy_1level.csv', function(data_1) {
-        console.log(data_1, data)
-        // stratify the data: reformatting for d3.js
-        var root = d3.stratify()
-            .id(function(d) { return d.Nom; })   // Name of the entity (column name is name in csv)
-            .parentId(function(d) { return d.parent; })   // Name of the parent (column name is parent in csv)
-            (data);
-        root.sum(function(d) { return d.indicateur })   // Compute the numeric value for each entity
-
-        // Then d3.treemap computes the position of each element of the hierarchy
-        // The coordinates are added to the root object above
-        d3.treemap()
-            .size([width, height])
-            .padding(4)
-            (root)
-
-        console.log(root.leaves())
-        // use this information to add rectangles:
-        svg
-            .selectAll("rect")
-            .data(root.leaves())
-            .enter()
-            .append("rect")
-            .attr('x', function (d) { return d.x0; })
-            .attr('y', function (d) { return d.y0; })
-            .attr('width', function (d) { return d.x1 - d.x0; })
-            .attr('height', function (d) { return d.y1 - d.y0; })
-            .style("stroke", "black")
-            .style("fill", "#69b3a2");
-
-        // and to add the text labels
-        svg
-            .selectAll("text")
-            .data(root.leaves())
-            .enter()
-            .append("text")
-            .attr("x", function(d){ return d.x0+10})    // +10 to adjust position (more right)
-            .attr("y", function(d){ return d.y0+20})    // +20 to adjust position (lower)
-            .text(function(d){ return d.data.name})
-            .attr("font-size", "15px")
-            .attr("fill", "white")
+    data = data.filter((d)=>{
+        return d.Marque === 'Dacia'
     })
+    data.push({Nom: 'origin', parent:'', indicateur:null})
+    console.log(data)
+    // stratify the data: reformatting for d3.js
+    var root = d3.stratify()
+        .id(function(d) { return d.Nom; })   // Name of the entity (column name is name in csv)
+        .parentId(function(d) { return d.parent; })   // Name of the parent (column name is parent in csv)
+        (data);
+    root.sum(function(d) { return d.indicateur })   // Compute the numeric value for each entity
+
+    // Then d3.treemap computes the position of each element of the hierarchy
+    // The coordinates are added to the root object above
+    d3.treemap()
+        .size([width, height])
+        .padding(4)
+        (root)
+
+    // use this information to add rectangles:
+    svg
+        .selectAll("rect")
+        .data(root.leaves())
+        .enter()
+        .append("rect")
+        .attr('x', function (d) { return d.x0; })
+        .attr('y', function (d) { return d.y0; })
+        .attr('width', function (d) { return d.x1 - d.x0; })
+        .attr('height', function (d) { return d.y1 - d.y0; })
+        .style("stroke", "black")
+        .style("fill", "#69b3a2");
+
+    // and to add the text labels
+    svg
+        .selectAll("text")
+        .data(root.leaves())
+        .enter()
+        .append("text")
+        .attr("x", function(d){ return d.x0+10})    // +10 to adjust position (more right)
+        .attr("y", function(d){ return d.y0+20})    // +20 to adjust position (lower)
+        .text(function(d){ return d.data.Nom + ': ' + Math.floor(d.data.indicateur)})
+        .attr("font-size", "15px")
+        .attr("fill", "white")
 }
 getTreeMap()
