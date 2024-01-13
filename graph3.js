d47daa6b12e9c249d0837006d70627daf65e8281
Popole <falqleopaul@gmail.com>
2024-01-13 13:54:34 +0100

    fix data 3

--- graph3.js
+++ graph3.js
@@ -8,7 +8,6 @@ function updateGraph3(marque){
         svgExists.remove();
     }
 
-// append the svg object to the body of the page
     var svg = d3.select(".graph3")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -17,26 +16,21 @@ function updateGraph3(marque){
         .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 
-// Read data
     let dataBis = data.filter((d)=>{
         return d.Marque === marque
     })
     dataBis.push({Nom: 'origin', parent:'', indicateur:null})
-    // stratify the data: reformatting for d3.js
     var root = d3.stratify()
-        .id(function(d) { return d.Nom; })   // Name of the entity (column name is name in csv)
-        .parentId(function(d) { return d.parent; })   // Name of the parent (column name is parent in csv)
+        .id(function(d) { return d.Nom; })
+        .parentId(function(d) { return d.parent; })
         (dataBis);
-    root.sum(function(d) { return d.indicateur })   // Compute the numeric value for each entity
+    root.sum(function(d) { return d.indicateur })
 
-    // Then d3.treemap computes the position of each element of the hierarchy
-    // The coordinates are added to the root object above
     d3.treemap()
         .size([width, height])
         .padding(4)
         (root)
 
-    // use this information to add rectangles:
     svg
         .selectAll("rect")
         .data(root.leaves())
@@ -64,8 +58,8 @@ function updateGraph3(marque){
 
             lines.forEach(function (line, i) {
                 text.append("tspan")
-                    .attr("x", d.x0 + 10) // Position X pour chaque tspan
-                    .attr("y", d.y0 + 20 + i * 20) // Position Y ajustée pour chaque tspan
+                    .attr("x", d.x0 + 10)
+                    .attr("y", d.y0 + 20 + i * 20)
                     .text(line);
             });
         });
